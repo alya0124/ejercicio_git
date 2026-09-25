8b22a6aa77e72dbae2f535d1844f5566af4b7398
alya0124 <23301542@uttt.edu.mx>
2026-09-25 10:30:21 -0600

    Solucion de main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
- return f"<h1>Bienvenido al portal universitario, {nombre}!</h1>"
+ return f"<h1>Bienvenido al portal universitario, Alan!</h1>"
 
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=5000)

532310901fb173dc7158e7c7929c521ee9098b6f
alya0124 <23301542@uttt.edu.mx>
2026-09-25 10:33:17 -0600

    Nueva ruta en main.py

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
- return f"<h1>Bienvenido al portal universitario, {nombre}!</h1>"
+ return f"<h1>Bienvenido al portal universitario, Alan!</h1>"
+
+@app.route('/api/status')
+def status():
+ return {"status": "ok", "entorno": "contenedor-docker", "version": "1.1.0"}
 
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=5000)
